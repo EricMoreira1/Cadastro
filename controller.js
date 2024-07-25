110ac681fe1d98c20661ddbfb8477f898a29363a
Eric Mendes Moreira <eric.moreira@alunoescolassociais.org.br>
2024-07-25 09:04:46 -0300

    Implanta a função de verificação de campos não preenchidos e manda um alert

---
+++ controller.js
@@ -0,0 +1,12 @@
+// validar acesso em tela de login
+function acessar(){
+    let loginEmail = document.getElementById('loginEmail').value;
+    let loginSenha = document.getElementById('loginSenha').value;
+ 
+    if(!loginEmail || !loginSenha){
+        alert("favor preencher todos os campos")
+    }else{
+                //alert("campos preenchidos com sucesso")
+                window.location.href = 'cadastro.html'
+            }
+     }
