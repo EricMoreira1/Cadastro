cd4a3e630d59be31b1023f6fc19c08a5adda04ea
Eric Mendes Moreira <eric.moreira@alunoescolassociais.org.br>
2024-08-29 09:18:59 -0300

    Adiciona ao código mais informações sobre os códigos utilizados

--- controller.js
+++ controller.js
@@ -21,40 +21,43 @@ function salvarUser() {
 
     // Validar email antes de continuar
     if (!checarEmail(emailUser)) {
-        return; // funciona se o email for inválido
+        return; // funciona caso o email informado seja inválido
     }
 
-    if (nomeUser && emailUser) {
-        dadosLista.push(nomeUser);
-        emailLista.push(emailUser);
+    if (nomeUser && emailUser) { // "&&" Faz com que as funções abaixo funcionem também ao email sem que precise reescrever o código duas vezes 
+        dadosLista.push(nomeUser); // envia o nome escrito para a tabela dadosLista
+        emailLista.push(emailUser); // envia o Email escrito para a tabela emailLista
         criaLista();
-        document.getElementById('nomeUser').value = "";
+        document.getElementById('nomeUser').value = ""; 
         document.getElementById('emailUser').value = "";
+        // verifica se os campos forma preenchidos
     } else {
-        alert("Favor informar o nome para cadastro e o Email");
+        alert("Favor informar o nome para cadastro e o Email"); // se não forem preenchidos o alert será ativo
     }
 }
 
 // FUNÇÃO PARA CRIAR LISTA
 function criaLista(){
-    let tabela = document.getElementById('tabela').innerHTML = "<tr><th>Nome Usuário</th><th>Email</th><th>Ações</th></tr>";
+    let tabela = document.getElementById('tabela').innerHTML = "<tr><th>Nome Usuário</th><th>Email</th><th>Ações</th></tr>"; // adiciona as variáveis a tela para serem realizadas as ações
     for(let i = 0; i <= (dadosLista.length-1); i++){
         tabela += "<tr><td>" + dadosLista[i] + "</td><td>" + emailLista[i] + "</td><td><button type='button' onclick='editar(this.parentNode.parentNode.rowIndex)'>Editar</button><button type='button' onclick='excluir(this.parentNode.parentNode.rowIndex)'>Excluir</button></td></tr>";
       // escreve códigos sem uso nescessário do HTML
-        document.getElementById('tabela').innerHTML = tabela;
+        document.getElementById('tabela').innerHTML = tabela; // Informa a tabela para a qual os dados serão enviados
     }
 }
  
+// FUNÇÃO PARA VALIDAR O EMAIL INFORMADO
 function checarEmail(email){
-    if (email == "" || 
-        email.indexOf("@") == -1 || 
-        email.indexOf(".") == -1) {
-        alert("Por favor informe um email válido");
+    if (email == "" || // Verifica se não há espaços no texto
+        email.indexOf("@") == -1 || // Verifica se tem @ no texto informado
+        email.indexOf(".") == -1) { // Verifica se tem . no texto informado
+        alert("Por favor informe um email válido"); // se não houver nenhum dos requisitos o alert será ativado
         return false;
     } else {
-        return true;
+        return true; // caso todos os requisitos sejam cumpridos o Email é válido
     }
 }
+
 
 // FUNÇÃO PARA EDITAR NOMES DA LISTA
 function editar(i){
@@ -69,9 +72,10 @@ function editar(i){
 // FUNÇÃO PARA EXCLUIR NOMES DA LISTA
 function excluir(i){
     dadosLista.splice((i - 1), 1);
-    // linha 46 (explicação do splice)
+    // linha 66 (explicação do splice)
     document.getElementById('tabela').deleteRow(i);
     // Apaga o contéudo escrito
     document.getElementById('nomeUser').value = "";
     document.getElementById('emailUser').value = "";
+    // Identifica o ID na tabela e exclui
 }
